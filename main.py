7d61cbfa2f36b4dcddf5d323940e51ae269fd7ef
Jacky7533 <jackyyuen753@gmail.com>
2024-08-29 13:34:28 -0700

    edit main.py, limit fps to 60, less CPU consumption

--- main.py
+++ main.py
@@ -13,13 +13,16 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_time = pygame.time.Clock()
+    dt = 0
     while pygame.get_init():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, color=(0,0,0))
         pygame.display.flip()
-
+        game_time.tick(60)
+        dt = game_time.tick()/1000
 
 if __name__ == "__main__":
     main()
